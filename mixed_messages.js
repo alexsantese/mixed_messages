74788c9d4ea2b8e4ad02c0fb107455cfbdec1672
Alex Santese <alexsantese@gmail.com>
2021-04-06 01:19:19 +0900

    First commit

---
+++ mixed_messages.js
@@ -0,0 +1,21 @@
+// Arrays that the function will pull from
+
+const quotes = ['The greatest glory in living lies not in never falling, but in rising everytime we fall. -Nelson Mandela', 'If life were predictable, it would cease to be life, and be without flavor. -Eleanor Roosevelt', 'Always remember that you are absolutely unique, just like everyone else. -Margaret Mead', 'Life is never fair, and perhaps it is a good thing for most of us that it is not. -Oscar Wilde', 'You only live once, but if you do it right, once is enough. -Mae West'];
+
+const fortune = ['']
+
+const zodiac = ['pisces', 'aquarius', 'aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagitarius', 'capricorn'];
+
+// Random selector function
+const randomizer = arr => {
+  let randNum = Math.floor(Math.random() * arr.length);
+  return arr[randNum];
+};
+
+// Rerurns a zodiac sign based on birth month
+const yourZodiac = (birthMonth) => {
+  switch (birthMonth) {
+    case 1:
+      return 
+  }
+}
